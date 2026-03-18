94a8b3e3fc446742833177442fbd91132f945174
NongHett0082 <ais0622656503@gmail.com>
2026-03-18 23:00:57 +0700

    Update socketClient.js

--- battleship/public/js/socketClient.js
+++ battleship/public/js/socketClient.js
@@ -1,13 +1,11 @@
-(function(window){
+(function(window) {
+  'use strict';
   let _socket = null;
-
+  
   const socketClient = {
-    connect() {
+    connect: function() {
       if (_socket && _socket.connected) return _socket;
-      
-      // ✅ Detect server URL automatically for production
       const serverUrl = window.location.origin;
-      
       _socket = io(serverUrl, {
         transports: ['polling', 'websocket'],
         reconnection: true,
@@ -17,32 +15,28 @@
         timeout: 20000,
         autoConnect: true
       });
-      
-      _socket.on('connect', () => console.log('[Socket] Connected:', _socket.id));
-      _socket.on('connect_error', (err) => console.error('[Socket] Error:', err.message));
-      _socket.on('disconnect', (r) => console.warn('[Socket] Disconnected:', r));
-      _socket.on('reconnect', () => console.log('[Socket] Reconnected:', _socket.id));
+      _socket.on('connect', function() { console.log('[Socket] Connected:', _socket.id); });
+      _socket.on('connect_error', function(err) { console.error('[Socket] Error:', err.message); });
+      _socket.on('disconnect', function(r) { console.warn('[Socket] Disconnected:', r); });
+      _socket.on('reconnect', function() { console.log('[Socket] Reconnected:', _socket.id); });
       return _socket;
     },
-    
-    get id() { return _socket?.id || null; },
-    get connected() { return _socket?.connected || false; },
-    
-    createRoom(n, max, cb) { _socket?.emit('createRoom', { playerName:n, maxPlayers:max }, cb); },
-    joinRoom(code, name, cb) { _socket?.emit('joinRoom', { roomCode:code, playerName:name }, cb); },
-    startSetup(room) { _socket?.emit('startSetup', { roomCode:room }); },
-    rejoinRoom(room, name) { _socket?.emit('rejoinRoom', { roomCode:room, playerName:name }); },
-    placeShip(room, ori, size, r, c) { _socket?.emit('placeShip', { roomCode:room, orientation:ori, size, startRow:r, startCol:c }); },
-    placeLand(room, r, c) { _socket?.emit('placeLand', { roomCode:room, row:r, col:c }); },
-    placeTurret(room, r, c) { _socket?.emit('placeTurret', { roomCode:room, row:r, col:c }); },
-    removeItem(room, r, c) { _socket?.emit('removeItem', { roomCode:room, row:r, col:c }); },
-    markReady(room) { _socket?.emit('markReady', { roomCode:room }); },
-    attack(room, target, r, c) { _socket?.emit('attack', { roomCode:room, targetId:target, row:r, col:c }); },
-    endTurn(room) { _socket?.emit('endTurn', { roomCode:room }); },
-    forfeit(room) { _socket?.emit('forfeit', { roomCode:room }); },
-    
-    on(e, cb) { _socket?.on(e, cb); },
-    off(e, cb) { _socket?.off(e, cb); },
+    get id() { return _socket ? _socket.id : null; },
+    get connected() { return _socket ? _socket.connected : false; },
+    createRoom: function(n, max, cb) { if (_socket) _socket.emit('createRoom', { playerName: n, maxPlayers: max }, cb); },
+    joinRoom: function(code, name, cb) { if (_socket) _socket.emit('joinRoom', { roomCode: code, playerName: name }, cb); },
+    startSetup: function(room) { if (_socket) _socket.emit('startSetup', { roomCode: room }); },
+    rejoinRoom: function(room, name) { if (_socket) _socket.emit('rejoinRoom', { roomCode: room, playerName: name }); },
+    placeShip: function(room, ori, size, r, c) { if (_socket) _socket.emit('placeShip', { roomCode: room, orientation: ori, size: size, startRow: r, startCol: c }); },
+    placeLand: function(room, r, c) { if (_socket) _socket.emit('placeLand', { roomCode: room, row: r, col: c }); },
+    placeTurret: function(room, r, c) { if (_socket) _socket.emit('placeTurret', { roomCode: room, row: r, col: c }); },
+    removeItem: function(room, r, c) { if (_socket) _socket.emit('removeItem', { roomCode: room, row: r, col: c }); },
+    markReady: function(room) { if (_socket) _socket.emit('markReady', { roomCode: room }); },
+    attack: function(room, target, r, c) { if (_socket) _socket.emit('attack', { roomCode: room, targetId: target, row: r, col: c }); },
+    endTurn: function(room) { if (_socket) _socket.emit('endTurn', { roomCode: room }); },
+    forfeit: function(room) { if (_socket) _socket.emit('forfeit', { roomCode: room }); },
+    on: function(e, cb) { if (_socket) _socket.on(e, cb); },
+    off: function(e, cb) { if (_socket) _socket.off(e, cb); }
   };
   
   window.SC = socketClient;
